f1a2f95f066358e464dbe7ac0bd75d3f0638a2d3
imacaveman <153271834+imacaveman@users.noreply.github.com>
2024-01-30 11:47:41 +0000

    992px carousel todo

--- js/myjs.js
+++ js/myjs.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('.carousel-container').slick({
       dots: true,
       infinite: true,
+      prevArrow: false,
+      nextArrow: false,
       speed: 300
     });
   });
