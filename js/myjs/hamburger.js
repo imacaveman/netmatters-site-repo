75ffa973ced00e1182c06ac89616cfe6a5f55ff3
imacaveman <153271834+imacaveman@users.noreply.github.com>
2024-02-28 08:31:12 +0000

    done some

--- js/myjs/hamburger.js
+++ js/myjs/hamburger.js
@@ -2,17 +2,20 @@
 $hamburgerContainer.on("click", function(event){
     $burger.toggleClass("is-active");
     $(this).toggleClass("burger-clicked");
+    $sideBar.toggleClass("sidebar-show");
     $bodyAll.toggleClass("allmove");
     $(".clickableoverlay").show();
-    $sideBar.toggleClass("sidebar-show");
-    $("body").css("overflow", "hidden");
 });
   
 $(".clickableoverlay").on("click", function(){
     $(this).hide();
     $burger.toggleClass("is-active");
-    $bodyAll.toggleClass("allmove");
     $sideBar.toggleClass("sidebar-show");
-    $("body").css("overflow", "auto");
+    $bodyAll.toggleClass("allmove");
+    $('body').off('scroll mousewheel touchmove', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+  });
 });
   
