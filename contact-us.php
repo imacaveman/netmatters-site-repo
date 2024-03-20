24f44802d4f763660adf6237988b1ce4b8546c6e
imacaveman <153271834+imacaveman@users.noreply.github.com>
2024-03-20 14:40:31 +0000

    continue setting up contact-us

--- contact-us.php
+++ contact-us.php
@@ -15,8 +15,78 @@
 </head>
 <body>
     <?php include "includes/header.php" ?>
-    <div class="our-offices-title">
-        <h2>Our Offices</h2>
+    <div class="page-title-container">
+        <div class="global-container">
+            <div class="page-title">
+                <h2>Our Offices</h2>
+            </div>
+        </div>
+    </div>
+    <div id="middle-content">
+        <div class="our-services">
+            <div class="global-container">
+                <div class="service-block">
+                    <a href="#" class="software-ref block block-top">
+                        <span class="icon soft-icn big-icn"><i class="fa-solid fa-laptop fa-2x"></i></span>
+                        <span class="block-title">Bespoke Software</span>
+                        <p class="desc">Bespoke software solutions for all your business needs including integrations and reporting.</p>
+                        <div class="btn-container">
+                            <span class="read-more rm-soft">Read More</span>
+                        </div>
+                    </a>
+                    <a href="#" class="support-ref block block-top">
+                        <span class="icon supp-icn big-icn"><i class="fa-solid fa-desktop fa-2x"></i></span>
+                        <span class="block-title">IT Support</span>
+                        <p class="desc">Fully managed IT support and consultancy packages tailored to meet your exact business needs.</p>
+                        <div class="btn-container">
+                            <span class="read-more rm-supp">Read More</span>
+                        </div>
+                    </a>
+                    <a href="#" class="marketing-ref block block-top">
+                        <span class="icon mrkt-icn big-icn"><i class="fa-solid fa-chart-simple fa-2x"></i></span>
+                        <span class="block-title">Digital Marketing</span>
+                        <p class="desc">Driven brand awareness & ROI through creative digital marketing campaigns.</p>
+                        <div class="btn-container">
+                            <span class="read-more rm-mrkt">Read More</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="service-block-med">
+                    <a href="#" class="telecoms-ref block block-med">
+                        <span class="icon tele-icn big-icn"><i class="fa-solid fa-phone-volume fa-2x"></i></span>
+                        <span class="block-title">Telecoms Services</span>
+                        <p class="desc">Business telephony solutions including mobile & connectivity solutions.</p>
+                        <div class="btn-container">
+                            <span class="read-more rm-tele">Read More</span>
+                        </div>
+                    </a>
+                    <a href="#" class="webdesign-ref block block-med">
+                        <span class="icon dsgn-icn big-icn"><i class="fa-solid fa-code fa-2x"></i></span>
+                        <span class="block-title">Web Design</span>
+                        <p class="desc">User-centric design for businesses looking to make a lasting impression.</p>
+                        <div class="btn-container">
+                            <span class="read-more rm-dsgn">Read More</span>
+                        </div>
+                    </a>
+                    <a href="#" class="security-ref block block-med">
+                        <span class="icon secure-icn big-icn"><i class="fa-solid fa-shield-halved fa-2x"></i></span>
+                        <span class="block-title">Cyber Security</span>
+                        <p class="desc">Prevention, testing, consultancy & breach management services.</p>
+                        <div class="btn-container">
+                            <span class="read-more rm-secure">Read More</span>
+                        </div>
+                    </a>
+                    <a href="#" class="training-ref block block-med">
+                        <span class="icon train-icn big-icn"><i class="fa-solid fa-graduation-cap fa-2x"></i></span>
+                        <span class="block-title">Developer Training</span>
+                        <p class="desc">Prevention, testing, consultancy & breach management services.</p>
+                        <div class="btn-container">
+                            <span class="read-more rm-train">Read More</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
     <?php include "includes/footer.php" ?>
     <?php include "includes/submenu.php" ?>
